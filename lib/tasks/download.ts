a9643bddfa7fc58f7106424c9a4abb0c2d5dfaf1
Lachlan Campbell <lachlanjc@hey.com>
2025-04-06 16:49:25 -0400

    Download more

--- lib/tasks/download.ts
+++ lib/tasks/download.ts
@@ -34,18 +34,50 @@ function parseArticle(url: string): Promise<string> {
   });
 }
 
-const scopedSites = SITES.filter((site) => site.state === "New Jersey");
+const scopedSites = SITES.filter((site) => site.id.startsWith("NJ"));
 // sort((a, b) => a.id.localeCompare(b.id))
 
-for (const site of scopedSites) {
+let stats: {
+  total: number;
+  empty: string[];
+  success: string[];
+  failure: string[];
+} = {
+  total: 0,
+  empty: [],
+  success: [],
+  failure: [],
+};
+
+async function downloadSite(site: Site): Promise<void> {
   const url = `https://cumulis.epa.gov/supercpad/SiteProfiles/index.cfm?fuseaction=second.cleanup&id=0${site.semsId}`;
+  stats.total++;
   console.log(`Downloading ${site.id} ${site.name}... ${url}`);
-  parseArticle(url)
-    .then((content) => {
-      Bun.write(`./lib/data/txt/${site.id}.txt`, content);
-      console.log(`Parsed content for ${site.id} ${site.name}`);
-    })
-    .catch((error) => {
-      console.error(`Error parsing ${site.id} ${site.name}: ${error}`);
-    });
+  let content = "";
+  try {
+    content = await parseArticle(url);
+  } catch (error) {
+    console.error(`Failed to download ${site.id} ${site.name}: ${error}`);
+    stats.failure.push(site.id);
+  }
+  if (content.length > 0) {
+    Bun.write(`./lib/data/txt/${site.id}.txt`, content);
+    console.log(`Parsed content for ${site.id} ${site.name}`);
+    stats.success.push(site.id);
+  } else {
+    console.error(`No content found for ${site.id} ${site.name}`);
+    stats.empty.push(site.id);
+  }
 }
+
+await Promise.all(scopedSites.map(downloadSite));
+
+console.log(`REPORT
+------
+Total: ${stats.total}
+Empty: ${stats.empty.length}
+Success: ${stats.success.length}
+Failure: ${stats.failure.length}
+---`);
+
+Bun.write("./lib/data/txt.json", JSON.stringify(stats, null, 2));
