9822fe1e6e7cbb2f48759cc10dd02b9f8c99f525
Lachlan Campbell <lachlanjc@hey.com>
2025-04-06 15:57:54 -0400

    Retry fetches

--- lib/tasks/download.ts
+++ lib/tasks/download.ts
@@ -3,24 +3,39 @@ import SITES from "@/lib/data/sites.json" assert { type: "json" };
 import { Defuddle } from "defuddle/node";
 import { JSDOM } from "jsdom";
 
+async function fetchWithRetry(url: string, retries = 3): Promise<Response> {
+  try {
+    return await fetch(url);
+  } catch (error) {
+    if (retries > 0) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return fetchWithRetry(url, retries - 1);
+    }
+    throw error;
+  }
+}
+
 function parseArticle(url: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
-    const dom = await JSDOM.fromURL(url);
+    const response = await fetchWithRetry(url).then((res) => res.text());
+    const dom = new JSDOM(response);
     const result = await Defuddle(dom, url, { markdown: true });
     const content = result.content ?? "";
-    // console.log(content);
+    // const id = url.split("=").at(-1);
+    if (content.length === 0) {
+      return reject("Empty");
+    }
     if (content.startsWith("Oops!")) {
       return reject("URL invalid");
     }
     const sections = content.split("\n\n---");
     const body = sections.slice(1, sections.length).join("").trim();
-
     resolve(body);
   });
 }
 
-const scopedSites = SITES.filter((site) => site.state === "Florida");
-// sort((a, b) => a.id.localeCompare(b.id)).slice(0, 10);
+const scopedSites = SITES.filter((site) => site.state === "New Jersey");
+// sort((a, b) => a.id.localeCompare(b.id))
 
 for (const site of scopedSites) {
   const url = `https://cumulis.epa.gov/supercpad/SiteProfiles/index.cfm?fuseaction=second.cleanup&id=0${site.semsId}`;
