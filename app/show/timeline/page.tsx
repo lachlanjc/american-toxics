396552bfa580d124ddfed203c4b3655d41e0d98c
Lachlan Campbell <lachlanjc@hey.com>
2025-05-03 20:41:07 -0400

    Split timeline

--- app/show/timeline/page.tsx
+++ app/show/timeline/page.tsx
@@ -4,18 +4,24 @@ import ReactMarkdown from "react-markdown";
 
 export const metadata = { title: "Timeline" };
 
+const files = {
+  "timeline-listed.md": "1981–1984",
+  "timeline2.md": "1989–2024",
+};
+
 export default async function Page() {
+  const file = 0;
   const filePath = path.join(
     process.cwd(),
     "lib",
     "data",
-    "timeline-listed.md",
+    Object.entries(files)[file][0],
   );
   const content = (await fs.readFile(filePath, "utf8")) || "";
   return (
-    <div className="prose font-mono prose-sm max-w-none">
-      <h1 className="text-6xl font-sans text-balance uppercase text-center mt-12 py-16">
-        Timeline of Superfund Sites by Listing Date
+    <div className="prose font-mono prose-sm max-w-none px-12">
+      <h1 className="text-6xl font-sans text-balance uppercase text-center mt-16 py-16">
+        Superfund Sites as&nbsp;Listed: {Object.entries(files)[file][1]}
       </h1>
       <ReactMarkdown>{content}</ReactMarkdown>
       <style>{`
